262f46b2c1ecca4fa184f9777b2a578a9047e13f
Anna Vasileva <vasilec1992@yandex.rum>
2021-03-31 11:28:38 +0200

    fetch function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+import "./App.css";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    async function fetchMyAPI() {
+      const response = await fetch(url);
+      const data = await response.json();
+      const [item] = data.results;
+      setData(item);
+      setLoading(false);
+    }
+
+    fetchMyAPI();
+  }, []);
+
+  return { data, loading };
+};
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const { data, loading } = useFetch("https://api.randomuser.me/");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+      {loading ? <div>...loading</div> : <div>{data.name.first}</div>}
     </div>
   );
-}
+};
 
 export default App;
